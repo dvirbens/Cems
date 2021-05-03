749894416cae8d7bdaa2dcde0e0100bd2eb7e27b
arikz-tech <54023820+arikz-tech@users.noreply.github.com>
2021-05-03 11:39:02 +0300

    import

--- CEMS/src/gui/TableGuiController.java
+++ CEMS/src/gui/TableGuiController.java
@@ -7,17 +7,14 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import models.Test;
 
 public class TableGuiController implements Initializable {
