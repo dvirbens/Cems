c8c629eec588aa02e1561ce587ae17536303de24
arikz-tech <54023820+arikz-tech@users.noreply.github.com>
2021-05-19 11:32:11 +0300

    update

--- CEMS/src/client/gui/ExamManagementController.java
+++ CEMS/src/client/gui/ExamManagementController.java
@@ -5,36 +5,84 @@ import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import com.jfoenix.controls.JFXButton;
+
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ExamManagementController implements Initializable {
 
 	@FXML
-	private Label lTime;
+	private static Label lTime;
 
 	@FXML
 	private Text tTime;
 
+	private static Label label;
+
 	public void start() {
 		try {
-			Pane examManagment = (Pane) FXMLLoader.load(getClass().getResource("ExamManagement.fxml"));
-			Scene scene = new Scene(examManagment, 500, 400);
+			VBox examManagement = new VBox();
+			setVBoxComponents(examManagement);
+			Scene scene = new Scene(examManagement, 500, 400);
 			Stage stage = new Stage();
 			stage.setScene(scene);
 			stage.setTitle("Exam Management");
 			stage.show();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	private void setVBoxComponents(VBox examManagement) {
+		label = new Label();
+		label.setPrefSize(515, 128);
+		label.setStyle("-fx-background-color:#333333;" + "-fx-text-fill:white;");
+		label.setAlignment(Pos.CENTER);
+		examManagement.setAlignment(Pos.CENTER);
+
+		JFXButton freezeExam = new JFXButton();
+		freezeExam.setPrefSize(200, 30);
+		freezeExam.setStyle("-fx-background-color:#48a832;" + "-fx-background-radius:10;" + "-fx-text-fill:white;");
+		freezeExam.setText("Freeze exam");
+		freezeExam.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+			}
+
+		});
+
+		JFXButton askForExstension = new JFXButton();
+		askForExstension.setPrefSize(200, 30);
+		askForExstension
+				.setStyle("-fx-background-color:#48a832;" + "-fx-background-radius:10;" + "-fx-text-fill:white;");
+		askForExstension.setText("Ask for time extension");
+		askForExstension.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+
+			}
+
+		});
+
+		examManagement.getChildren().add(label);
+		examManagement.getChildren().add(freezeExam);
+		examManagement.getChildren().add(askForExstension);
+		Stopwatch sw = new Stopwatch(3);
+		sw.startTime();
 	}
 
 	public class Stopwatch {
@@ -54,15 +102,21 @@ public class ExamManagementController implements Initializable {
 			timer.scheduleAtFixedRate(new TimerTask() {
 
 				public void run() {
-					System.out.println(String.format("%02d:%02d\n", min, sec));
-					if (min == 0 && sec == 0) {
-						timer.cancel();
-					} else if (sec == 0) {
-						min--;
-						sec = 59;
-					} else {
-						sec--;
-					}
+					Platform.runLater(new Runnable() {
+
+						@Override
+						public void run() {
+							label.setText(String.format("%02d:%02d\n", min, sec));
+							if (min == 0 && sec == 0) {
+								timer.cancel();
+							} else if (sec == 0) {
+								min--;
+								sec = 59;
+							} else {
+								sec--;
+							}
+						}
+					});
 				}
 			}, delay, period);
 		}
