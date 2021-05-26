895670913b568178a19f052b5b4bbdfbdd05ffb0
yaakovsh8 <yaakovsh8@gmail.com>
2021-05-26 20:22:31 +0300

    principle gui

--- CEMS/src/client/gui/OverallStatistic.java
+++ CEMS/src/client/gui/OverallStatistic.java
@@ -1,9 +1,13 @@
 package client.gui;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXRadioButton;
+import com.sun.corba.se.spi.orb.Operation;
 
 import client.Client;
 import javafx.event.ActionEvent;
@@ -14,10 +18,15 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 
-public class OverallStatistic implements Initializable {
+
+public class OverallStatistic implements Initializable{
 
 	@FXML
 	private Pane PaneOverallStatistic;
@@ -27,6 +36,21 @@ public class OverallStatistic implements Initializable {
 
 	@FXML
 	private JFXComboBox<String> courseSelect;
+	
+	 @FXML
+	    private JFXButton displayBtn;
+
+    @FXML
+    private ToggleGroup statisticBy;
+	
+	@FXML
+	private JFXRadioButton slct_course;
+
+	@FXML
+	private JFXRadioButton slct_teacher;
+
+	@FXML
+	private JFXRadioButton slct_student;
 
 	@FXML
 	private CategoryAxis x_Exam;
@@ -36,82 +60,149 @@ public class OverallStatistic implements Initializable {
 
 	@FXML
 	private BarChart<String, Number> graph;
+	
+    @FXML
+    private JFXTextArea statisticNavigation;
+    
+    @FXML
+    private HBox HboxFiltter;
+    
+    @FXML
+    private JFXTextField teacherFiltter;
 
+    @FXML
+    private JFXTextField studentFiltter;
+    
+    @FXML
+    void studentFiltteringBy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void teacherFiltteringBy(ActionEvent event) {
+
+    }
+	
+    @FXML
+    private VBox VboxGraph;
+	public enum Operation {COURSE,STUDENT,TEACHER};
+	private Operation op = Operation.COURSE;
+	public enum StatisticBy {StatisticByCourse,StatisticByTeacher,StatisticByStudent};
+	private Series<String, Number> set = new XYChart.Series<String, Number>();
+	
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		courseSelect.getItems().addAll(Client.getSubjectCollection().getCourses());
-		
+	  	teacherFiltter.setVisible(false);
+    	studentFiltter.setVisible(false);
+		courseSelect.setVisible(false);
 	}
 
 	@SuppressWarnings("unchecked")
 	@FXML
-	void diplayGrapgh(ActionEvent event) {
-//			display data on the graph:
-		Series<String, Number> set1 = new XYChart.Series<String, Number>();
+	void diplayGrapgh(ActionEvent event){
+				if(!set.getData().isEmpty())
+					set.getData().clear();
+				
+//				fix the Bug in the Chart Bar!!
+				x_Exam.setAnimated(false);
+				graph.getData().clear();
+				graph.getData().addAll(set);
+				x_Exam.setAnimated(false);
+				graph.getData().clear();
 
-		set1.getData().add(new XYChart.Data<String, Number>("Algebra", 80));
-		set1.getData().add(new XYChart.Data<String, Number>("Phyton", 90));
-		set1.getData().add(new XYChart.Data<String, Number>("java", 100));
-		set1.getData().add(new XYChart.Data<String, Number>("Alge", 50));
-		set1.getData().add(new XYChart.Data<String, Number>("Phyt", 20));
-		set1.getData().add(new XYChart.Data<String, Number>("jav", 10));
-
-		graph.getData().addAll(set1);
-		
-//			set1.getData().add(new Data<String, Number>("alge", 92633.68));  
-//			set1.getData().add(new Data<String, Number>("ufva",52633.68));
-
-//			graph.getData().addAll(set1);
-//			graph.setBarGap(10.0);
-//			graph.getXAxis().setTickLabelGap(20);
-
-//			getData().addAll(set1);
-
+		    	switch (op) {
+					case COURSE:
+						
+						set.getData().add(new XYChart.Data<String, Number>("Algebra", 80));
+						set.getData().add(new XYChart.Data<String, Number>("Phyton", 90));
+						set.getData().add(new XYChart.Data<String, Number>("java", 100));
+						set.getData().add(new XYChart.Data<String, Number>("Math", 50));
+						graph.getData().addAll(set);
+						
+						break;
+					case STUDENT:
+						set.getData().add(new XYChart.Data<String, Number>("JACOB", 85));
+						set.getData().add(new XYChart.Data<String, Number>("ARIK", 90));
+						set.getData().add(new XYChart.Data<String, Number>("DVIR", 100));
+						set.getData().add(new XYChart.Data<String, Number>("SHENHAV", 70));
+						set.getData().add(new XYChart.Data<String, Number>("AVIEL", 80));
+						graph.getData().addAll(set);
+						
+						break;
+					case TEACHER:						
+						set.getData().add(new XYChart.Data<String, Number>("Avi", 80));
+						set.getData().add(new XYChart.Data<String, Number>("Katarina", 60));
+						set.getData().add(new XYChart.Data<String, Number>("Anat", 70));
+						graph.getData().addAll(set);
+						
+						break;
+					default:
+						break;
+		    	}
 	}
+	
+    @FXML
+    void additional_filterByCourse(ActionEvent event) {
+    	
+    }
+    
+    @FXML
+    void StatisticNoteNavigation(ActionEvent event) {
+    	
+    }
 
-	@FXML
-	void handleCourseBtn(ActionEvent event) {
+	 @FXML
+	    void statisticByCourse(ActionEvent event) { 
+		 	statisticNavigation.clear();
+		 	statisticNavigation.appendText("Selection of statistics by course, shows the grades of all students in the course.");
+	    	StatisticNoteNavigation(event);
+	    	
+	    	courseSelect.setVisible(true);
+	    	teacherFiltter.setVisible(false);
+	    	studentFiltter.setVisible(false);
+		 	op=Operation.COURSE;
+		 	
+//			fix the Bug in the Chart Bar!!
+			x_Exam.setAnimated(false);
+			graph.getData().clear();
+		 		
+	    }
 
-	}
+	    @FXML
+	    void statisticByStudent(ActionEvent event) {
+	    	statisticNavigation.clear();
+	    	statisticNavigation.appendText("Selection of statistics by student, shows all the grades that the student received");
+	    	StatisticNoteNavigation(event);
+	    	
+	    	studentFiltter.setVisible(true);
+	    	courseSelect.setVisible(false);
+	    	teacherFiltter.setVisible(false);
+	    	
+	    	op=Operation.STUDENT;
+	    	
+//			fix the Bug in the Chart Bar!!
+			x_Exam.setAnimated(false);
+			graph.getData().clear();
+			 		
+	    }
 
-	void diplayOnGrapgh(ActionEvent event) {
-
-	}
-
-//	    @FXML
-//	    void diplayOnGrapgh(ActionEvent event) {
-//	         final BarChart<String,Number> bc = 
-//	             new BarChart<String,Number>(x_Exam,y_Grades);
-//	         bc.setTitle("Country Summary");
-//	         x_Exam.setLabel("Country");       
-//	         y_Grades.setLabel("Value");
-//	  
-//	         XYChart.Series series1 = new XYChart.Series();
-//	         series1.setName("2003");       
-//	         series1.getData().add(new XYChart.Data("austria", 25601.34));
-//	         series1.getData().add(new XYChart.Data("brazil", 20148.82));
-//	         series1.getData().add(new XYChart.Data("france", 10000));
-//	         series1.getData().add(new XYChart.Data("italy", 35407.15));
-//	         series1.getData().add(new XYChart.Data("usa", 12000));      
-//	         
-//	         XYChart.Series series2 = new XYChart.Series();
-//	         series2.setName("2004");
-//	         series2.getData().add(new XYChart.Data("austria", 57401.85));
-//	         series2.getData().add(new XYChart.Data("brazil", 41941.19));
-//	         series2.getData().add(new XYChart.Data("france", 45263.37));
-//	         series2.getData().add(new XYChart.Data("italy", 117320.16));
-//	         series2.getData().add(new XYChart.Data("usa", 14845.27));  
-//	         
-//	         XYChart.Series series3 = new XYChart.Series();
-//	         series3.setName("2005");
-//	         series3.getData().add(new XYChart.Data("austria", 45000.65));
-//	         series3.getData().add(new XYChart.Data("brazil", 44835.76));
-//	         series3.getData().add(new XYChart.Data("france", 18722.18));
-//	         series3.getData().add(new XYChart.Data("italy", 17557.31));
-//	         series3.getData().add(new XYChart.Data("usa", 92633.68));  
-//	         
-//	         bc.getData().addAll(series1, series2, series3);
-//	         
-//	    }
-
+	    @FXML
+	    void statisticByTeacher(ActionEvent event) {
+	    	statisticNavigation.clear();
+	    	statisticNavigation.appendText("Selection of statistics by teacher, shows the scores of all exams belonging to the same teacher");
+	    	StatisticNoteNavigation(event);
+	    	
+	    	studentFiltter.setVisible(false);
+	    	courseSelect.setVisible(false);
+	    	teacherFiltter.setVisible(true);
+	    	
+	    	op=Operation.TEACHER;
+	    	
+//			fix the Bug in the Chart Bar!!
+			x_Exam.setAnimated(false);
+			graph.getData().clear();
+			 		
+	    }
 }
