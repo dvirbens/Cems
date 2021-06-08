369c8bfb2b44e3c7c7f8b9ea3f3b37fc98fcd434
yaakovsh8 <yaakovsh8@gmail.com>
2021-06-08 08:32:13 +0300

    PRINCIPLE

---
+++ CEMS/src/client/gui/PrincipleTimeExtention.java
@@ -0,0 +1,43 @@
+package client.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.sun.corba.se.spi.activation.Server;
+
+import client.Client;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PrincipleTimeExtention  implements Initializable {
+
+	    @FXML
+	    private TableView<?> tableExt;
+
+	    @FXML
+	    private TableColumn<?, ?> teacherCol;
+
+	    @FXML
+	    private TableColumn<?, ?> durationCol;
+
+	    @FXML
+	    private TableColumn<?, ?> extentionCol;
+
+	    @FXML
+	    private TableColumn<?, ?> viewCol;
+
+	    public enum RequsetToServer {
+			ExamExtentionRequest
+		};
+		
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+
+			tableExt.getItems().addAll(Server.g)
+//			addAll(Client.getSubjectCollection().getCourses());
+
+		}
+
+}
