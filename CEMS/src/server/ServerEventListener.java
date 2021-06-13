c6a53449f08363142f2719b8eb7f686bc1adb075
aviel817 <aviel1440@gmail.com>
2021-06-13 17:22:47 +0300

    server2

--- CEMS/src/server/ServerEventListener.java
+++ CEMS/src/server/ServerEventListener.java
@@ -3,8 +3,6 @@ package server;
 /**
  * Interface that demonstrate Server event listener in order to handle events
  * that occurred by server.
- * 
- * @author Arikz ,Dvir ben simon
  */
 public interface ServerEventListener {
 	void printToLog(String logs);
