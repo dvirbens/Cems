c2a0c906186238d79e80ee6282efc462fa3f41d5
aviel817 <aviel1440@gmail.com>
2021-06-14 12:20:36 +0300

    comments

--- CEMS/src/models/ExamProcess.java
+++ CEMS/src/models/ExamProcess.java
@@ -7,8 +7,6 @@ import ocsf.server.ConnectionToClient;
 /**
  * Class that represent the exam process while a teacher start an exam.
  * 
- * @author Shenhav , Aviel
- *
  */
 
 public class ExamProcess implements Serializable {
@@ -30,9 +28,19 @@ public class ExamProcess implements Serializable {
 		Computerized, Manual
 	}
 
-/****************************** Constructors ***********************************/
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor for ExamProcess
+	 * @param examId
+	 * @param date
+	 * @param time
+	 * @param teacherID
+	 * @param code
+	 * @param subject
+	 * @param course
+	 * @param duration
+	 */
 	public ExamProcess(String examId, String date, String time, String teacherID, String code, String subject,
 			String course, String duration) {
 		super();
@@ -47,6 +55,18 @@ public class ExamProcess implements Serializable {
 		this.type = ExamType.Computerized;
 	}
 
+	/**
+	 * Constructor2 for ExamProcess
+	 * @param examId
+	 * @param date
+	 * @param time
+	 * @param teacherID
+	 * @param code
+	 * @param subject
+	 * @param course
+	 * @param duration
+	 * @param manualFile
+	 */
 	public ExamProcess(String examId, String date, String time, String teacherID, String code, String subject,
 			String course, String duration, WordFile manualFile) {
 		super();
@@ -62,6 +82,10 @@ public class ExamProcess implements Serializable {
 		this.type = ExamType.Manual;
 	}
 
+	/**
+	 * Constructor3 for ExamProcess
+	 * @param examProcess
+	 */
 	public ExamProcess(ExamProcess examProcess) {
 		super();
 		this.examId = examProcess.getExamId();
@@ -76,111 +100,205 @@ public class ExamProcess implements Serializable {
 		this.type = examProcess.getType();
 	}
 	
-	/***************************** Setters and getters **********************************/
+	
+	/**
+	 * @return teacher id
+	 */
 	public String getTeacherID() {
 		return teacherID;
 	}
 
+	/**
+	 * set teacher id
+	 * @param teacherID
+	 */
 	public void setTeacherID(String teacherID) {
 		this.teacherID = teacherID;
 	}
 
+	/**
+	 * @return serial version
+	 */
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
+	/**
+	 * @return date
+	 */
 	public String getDate() {
 		return date;
 	}
 
+	/**
+	 * set date
+	 * @param date
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
 
+	/**
+	 * @return exam code
+	 */
 	public String getCode() {
 		return code;
 	}
 
+	/**
+	 * set exam code
+	 * @param code
+	 */
 	public void setCode(String code) {
 		this.code = code;
 	}
 
+	/**
+	 * set exam id
+	 * @param examId
+	 */
 	public void setexamId(String examId) {
 		this.examId = examId;
 	}
 
+	/**
+	 * @return exam type
+	 */
 	public ExamType getType() {
 		return type;
 	}
 
+	/**
+	 * set exam type
+	 * @param type
+	 */
 	public void setType(ExamType type) {
 		this.type = type;
 	}
 
+	/**
+	 * @return manual test file
+	 */
 	public WordFile getManualFile() {
 		return manualFile;
 	}
 
+	/**
+	 * set manual test file
+	 * @param manualFile
+	 */
 	public void setManualFile(WordFile manualFile) {
 		this.manualFile = manualFile;
 	}
 
+	/**
+	 * @return time
+	 */
 	public String getTime() {
 		return time;
 	}
 
+	/**
+	 * set time
+	 * @param time
+	 */
 	public void setTime(String time) {
 		this.time = time;
 	}
 
+	/**
+	 * @return exam id
+	 */
 	public String getExamId() {
 		return examId;
 	}
 
+	/**
+	 * set exam id
+	 * @param examId
+	 */
 	public void setExamId(String examId) {
 		this.examId = examId;
 	}
 
+	/**
+	 * @return subject
+	 */
 	public String getSubject() {
 		return subject;
 	}
 
+	/**
+	 * set subject
+	 * @param subject
+	 */
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
 
+	/**
+	 * @return course
+	 */
 	public String getCourse() {
 		return course;
 	}
 
+	/**
+	 * set course
+	 * @param course
+	 */
 	public void setCourse(String course) {
 		this.course = course;
 	}
 
+	/**
+	 * @return duration
+	 */
 	public String getDuration() {
 		return duration;
 	}
 
+	/**
+	 * set duration
+	 * @param duration
+	 */
 	public void setDuration(String duration) {
 		this.duration = duration;
 	}
 
+	/**
+	 * @return teacher client
+	 */
 	public ConnectionToClient getTeacherClient() {
 		return teacherClient;
 	}
 
+	/**
+	 * set teacher client
+	 * @param teacherClient
+	 */
 	public void setTeacherClient(ConnectionToClient teacherClient) {
 		this.teacherClient = teacherClient;
 	}
 
+	/**
+	 * @return count of finished students
+	 */
 	public String getFinishedStudentsCount() {
 		return finishedStudentsCount;
 	}
 
+	/**
+	 * set count of finished students
+	 * @param finishedStudentsCount
+	 */
 	public void setFinishedStudentsCount(String finishedStudentsCount) {
 		this.finishedStudentsCount = finishedStudentsCount;
 	}
 
+	/**
+	 * To string function
+	 */
 	@Override
 	public String toString() {
 		return "ExamProcess [examId=" + examId + ", date=" + date + ", time=" + time + ", teacherID=" + teacherID
