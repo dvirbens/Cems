0ebf486b5bd23ae122ed47bc6529f51d0ee0aa95
aviel817 <aviel1440@gmail.com>
2021-06-14 12:29:36 +0300

    comments

--- CEMS/src/models/ExecutedExam.java
+++ CEMS/src/models/ExecutedExam.java
@@ -7,8 +7,6 @@ import com.jfoenix.controls.JFXButton;
 /**
  * Class that  represent all the information after executed an exam 
  * (happens after the teacher freeze - close the exam or at the end of time given).
- * 
- * @author Shenhav , Aviel
  *
  */
 public class ExecutedExam implements Serializable {
@@ -29,7 +27,22 @@ public class ExecutedExam implements Serializable {
 	private JFXButton questionList;
 	private JFXButton gradeApproval;
 
-/****************************** Constructors ************************************/
+
+	/**
+	 * Constructor for ExecutedExam 
+	 * @param id
+	 * @param subject
+	 * @param course
+	 * @param teacherID
+	 * @param executorTeacherName
+	 * @param execDate
+	 * @param execTime
+	 * @param finishedStudentsCount
+	 * @param testType
+	 * @param avg
+	 * @param median
+	 * @param approved
+	 */
 	public ExecutedExam(String id, String subject, String course, String teacherID, String executorTeacherName,
 			String execDate, String execTime, String finishedStudentsCount, String testType, double avg, double median,
 			boolean approved) {
@@ -49,6 +62,15 @@ public class ExecutedExam implements Serializable {
 
 	}
 
+	/**
+	 * Constructor2 for ExecutedExam 
+	 * @param id
+	 * @param subject
+	 * @param course
+	 * @param execDate
+	 * @param testType
+	 * @param grade
+	 */
 	public ExecutedExam(String id, String subject, String course, String execDate, String testType, int grade) {
 		super();
 		this.id = id;
@@ -58,6 +80,19 @@ public class ExecutedExam implements Serializable {
 		this.testType = testType;
 	}
 
+	/**
+	 * Constructor3 for ExecutedExam 
+	 * @param id
+	 * @param executorTeacherName
+	 * @param teacherID
+	 * @param subject
+	 * @param course
+	 * @param execDate
+	 * @param execTime
+	 * @param testType
+	 * @param approved
+	 * @param avg
+	 */
 	public ExecutedExam(String id, String executorTeacherName, String teacherID, String subject, String course,
 			String execDate, String execTime, String testType, boolean approved, double avg) {
 		super();
@@ -73,6 +108,16 @@ public class ExecutedExam implements Serializable {
 		this.avg = avg;
 	}
 
+	/**
+	 * Constructor4 for ExecutedExam 
+	 * @param id
+	 * @param teacherID
+	 * @param executorTeacherName
+	 * @param execDate
+	 * @param execTime
+	 * @param avg
+	 * @param median
+	 */
 	public ExecutedExam(String id, String teacherID, String executorTeacherName, String execDate, String execTime,
 			double avg, double median) {
 		this.id = id;
@@ -84,6 +129,18 @@ public class ExecutedExam implements Serializable {
 		this.median = median;
 	}
 
+	/**
+	 * Constructor5 for ExecutedExam 
+	 * @param id
+	 * @param subject
+	 * @param course
+	 * @param teacherID
+	 * @param execDate
+	 * @param testType
+	 * @param avg
+	 * @param median
+	 * @param approved
+	 */
 	public ExecutedExam(String id, String subject, String course, String teacherID, String execDate, String testType,
 			double avg, double median, boolean approved) {
 		super();
@@ -98,123 +155,227 @@ public class ExecutedExam implements Serializable {
 		this.approved = approved;
 	}
 
-/***************************** Setters and getters **********************************/
+
+	/**
+	 * @return average
+	 */
 	public double getAvg() {
 		return avg;
 	}
 
+	/**
+	 * set average
+	 * @param avg
+	 */
 	public void setAvg(double avg) {
 		this.avg = avg;
 	}
 
+	/**
+	 * @return median
+	 */
 	public double getMedian() {
 		return median;
 	}
 
+	/**
+	 * set median
+	 * @param median
+	 */
 	public void setMedian(double median) {
 		this.median = median;
 	}
 
+	/**
+	 * @return id
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * set id
+	 * @param id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return subject
+	 */
 	public String getSubject() {
 		return subject;
 	}
 
+	/**
+	 * set subject
+	 * @param subject
+	 */
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
 
+	/**
+	 * @return course
+	 */
 	public String getCourse() {
 		return course;
 	}
 
+	/**
+	 * set course
+	 * @param course
+	 */
 	public void setCourse(String course) {
 		this.course = course;
 	}
 
+	/**
+	 * @return execution date
+	 */
 	public String getExecDate() {
 		return execDate;
 	}
 
+	/**
+	 * set execution date
+	 * @param execDate
+	 */
 	public void setExecDate(String execDate) {
 		this.execDate = execDate;
 	}
 
+	/**
+	 * @return test type
+	 */
 	public String getTestType() {
 		return testType;
 	}
 
+	/**
+	 * set test type 
+	 * @param testType
+	 */
 	public void setTestType(String testType) {
 		this.testType = testType;
 	}
 
+	/**
+	 * @return serial version
+	 */
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
+	/**
+	 * @return teacher id
+	 */
 	public String getTeacherID() {
 		return teacherID;
 	}
 
+	/**
+	 * set teacher id
+	 * @param teacherID
+	 */
 	public void setTeacherID(String teacherID) {
 		this.teacherID = teacherID;
 	}
 
+	/**
+	 * @return executor teacher name
+	 */
 	public String getExecutorTeacherName() {
 		return executorTeacherName;
 	}
 
+	/**
+	 * set executor teacher name
+	 * @param executorTeacherName
+	 */
 	public void setExecutorTeacherName(String executorTeacherName) {
 		this.executorTeacherName = executorTeacherName;
 	}
 
+	/**
+	 * @return total execution time
+	 */
 	public String getExecTime() {
 		return execTime;
 	}
 
+	/**
+	 * set total execution time
+	 * @param execTime
+	 */
 	public void setExecTime(String execTime) {
 		this.execTime = execTime;
 	}
 
+	/**
+	 * @return question list button
+	 */
 	public JFXButton getQuestionList() {
 		return questionList;
 	}
 
+	/**
+	 * set question list button
+	 * @param questionList
+	 */
 	public void setQuestionList(JFXButton questionList) {
 		this.questionList = questionList;
 	}
 
+	/**
+	 * @return button of grade approval
+	 */
 	public JFXButton getGradeApproval() {
 		return gradeApproval;
 	}
 
+	/**
+	 * set button of grade approval
+	 * @param gradeApproval
+	 */
 	public void setGradeApproval(JFXButton gradeApproval) {
 		this.gradeApproval = gradeApproval;
 	}
 
+	/**
+	 * @return count of finished students
+	 */
 	public String getFinishedStudentsCount() {
 		return finishedStudentsCount;
 	}
 
+	/**
+	 * set count of finished students
+	 * @param finishedStudentsCount
+	 */
 	public void setFinishedStudentsCount(String finishedStudentsCount) {
 		this.finishedStudentsCount = finishedStudentsCount;
 	}
 
+	/**
+	 * @return approved
+	 */
 	public boolean isApproved() {
 		return approved;
 	}
 
+	/**
+	 * set approved
+	 * @param approved
+	 */
 	public void setApproved(boolean approved) {
 		this.approved = approved;
 	}
 
+	/**
+	 * To string function
+	 */
 	@Override
 	public String toString() {
 		return "ExecutedExam [id=" + id + ", subject=" + subject + ", course=" + course + ", teacherID=" + teacherID
--- CEMS/src/models/StudentInExam.java
+++ CEMS/src/models/StudentInExam.java
@@ -7,8 +7,6 @@ import ocsf.server.ConnectionToClient;
 
 /**
  * Class that  represent a specific Student in a specific exam.
- * 
- * @author Shenhav , Aviel
  *
  */
 
@@ -23,7 +21,15 @@ public class StudentInExam implements Serializable {
 	private String[] solution;
 	private boolean finished;
 
-/****************************** Constructors ************************************/
+	
+	/**
+	 * Constructor
+	 * @param studentID
+	 * @param code
+	 * @param grade
+	 * @param execDuration
+	 * @param solution
+	 */
 	public StudentInExam(String studentID, String code, String grade, String execDuration, String[] solution) {
 		super();
 		this.studentID = studentID;
@@ -33,69 +39,126 @@ public class StudentInExam implements Serializable {
 		this.solution = solution;
 	}
 
+	/**
+	 * Constructor
+	 * @param studentID
+	 * @param client
+	 */
 	public StudentInExam(String studentID, ConnectionToClient client) {
 		super();
 		this.studentID = studentID;
 		this.client = client;
 	}
 
-/***************************** Setters and getters **********************************/
+
+	/**
+	 * @return studentID
+	 */
 	public String getStudentID() {
 		return studentID;
 	}
 
+	/**
+	 * set studentID
+	 * @param studentID
+	 */
 	public void setStudentID(String studentID) {
 		this.studentID = studentID;
 	}
 
+	/**
+	 * @return client
+	 */
 	public ConnectionToClient getClient() {
 		return client;
 	}
 
+	/**
+	 * set Client
+	 * @param client
+	 */
 	public void setClient(ConnectionToClient client) {
 		this.client = client;
 	}
 
+	/**
+	 * @return solutions
+	 */
 	public String[] getSolution() {
 		return solution;
 	}
 
+	/**
+	 * set Solutions
+	 * @param solution
+	 */
 	public void setSolution(String[] solution) {
 		this.solution = solution;
 	}
 
+	/**
+	 * @return finished
+	 */
 	public boolean isFinished() {
 		return finished;
 	}
 
+	/**
+	 * set finished
+	 * @param finished
+	 */
 	public void setFinished(boolean finished) {
 		this.finished = finished;
 	}
 
+	/**
+	 * @return code
+	 */
 	public String getCode() {
 		return code;
 	}
 
+	/**
+	 * set code
+	 * @param code
+	 */
 	public void setCode(String code) {
 		this.code = code;
 	}
 
+	/**
+	 * @return grade
+	 */
 	public String getGrade() {
 		return grade;
 	}
 
+	/** 
+	 * set grade
+	 * @param grade
+	 */
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
 	
+	/**
+	 * @return execDuration
+	 */
 	public String getExecDuration() {
 		return execDuration;
 	}
 
+	/**
+	 * Set ExecDuration
+	 * @param execDuration
+	 */
 	public void setExecDuration(String execDuration) {
 		this.execDuration = execDuration;
 	}
 
+	/**
+	 *toString
+	 */
 	@Override
 	public String toString() {
 		return "StudentInExam [studentID=" + studentID + ", code=" + code + ", grade=" + grade + ", client=" + client
